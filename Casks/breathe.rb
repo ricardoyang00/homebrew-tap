5881bfc5af8112e346fa22d5dd701523dc1773f8
Ricardo Yang <ricardo.yang00@gmail.com>
2026-05-06 23:05:03 +0100

    breathe v1.2.1

--- Casks/breathe.rb
+++ Casks/breathe.rb
@@ -1,6 +1,6 @@
 cask "breathe" do
-    version "1.2.0"
-    sha256 "062ff8f34c9670cdda1e163437bc6cd2756e77222b0507186b7148c7f7697ca4"
+    version "1.2.1"
+    sha256 "1434a1dfd0e139a92eeb78f5344a51c23661ffe28ee9e18af13439c6594fecff"
     url "https://github.com/ricardoyang00/breathe/releases/download/v#{version}/Breathe.dmg"
     name "Breathe"
     desc "Lightweight macOS menu bar app to monitor pollen and air quality"
